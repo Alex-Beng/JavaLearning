89df5398037f1102a14f042c78adbc5ed7bbddad
Alex Beng <599267065@qq.com>
2019-02-25 17:00:05 +0800

    operator partly done

---
+++ Operator/Operator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/**
+ * Operator
+ */
+public class Operator {
+    public static void main(String[] args) {
+        // 按运算符从高到低
+        // 从上到下进行
+
+        /// 后缀运算符 () [] . 
+
+        /// 一元运算符 ++ -- ！〜
+    }
+}
